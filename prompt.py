fc5d41fc12a53e47672fb58fd43e3096cbb5a2c9
Yasaswi Narukulla <narukullayasaswi06@gmail.com>
2026-07-06 21:21:40 +0530

    Add files via upload

---
+++ prompt.py
@@ -0,0 +1,21 @@
+from langchain_core.prompts import PromptTemplate
+blog_prompt=PromptTemplate( 
+    input_variables=["topic","tone","length","audience"],
+    template="""
+You are an expert content writer.
+
+Write a {length} blog post.
+Topic:{topic}
+Tone:{tone}
+Audience:{audience}
+Include:
+- Catchy Title
+- Introduction
+- 4-5 headings
+- Pracical examples
+- Conclusion
+- Key takeways
+
+Return the response in Markdown.
+"""
+)
